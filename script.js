3ebe4abd7ccbff89dfdcf0fc3ee15a8e002f89ba
Eduardo <edumarcondes93@hotmail.com>
2024-10-06 18:41:06 -0300

    Atualizando bug HTTP

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 document.getElementById('form-numeros').addEventListener('submit', function(event) {
+    event.preventDefault();
+
     const campoA = parseFloat(document.getElementById('campoA').value);
     const campoB = parseFloat(document.getElementById('campoB').value);
     const errorMessage = document.getElementById('error-message');
 
+    console.log("Campo A: ", campoA);
+    console.log("Campo B: ", campoB);
+
+    if (isNaN(campoA) || isNaN(campoB)) {
+        console.log("Um dos campos está vazio ou não é um número.");
+        errorMessage.textContent = "Por favor, insira valores válidos nos dois campos.";
+        errorMessage.style.display = 'block';
+        return;
+    }
+
     if (campoB <= campoA) {
+        console.log("Erro: Campo B é menor ou igual a Campo A.");
         errorMessage.style.display = 'block'; // Exibe a mensagem de erro
-        event.preventDefault();
     } else {
+        console.log("Sucesso: Campo B é maior que Campo A.");
         errorMessage.style.display = 'none'; // Oculta a mensagem de erro
+
+        alert(`Sucesso! O campo B (${campoB}) é maior que o campo A (${campoA}).`)
+        document.getElementById('form-numeros').reset();
     }
 });
